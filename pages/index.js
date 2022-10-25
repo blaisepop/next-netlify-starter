9c6305cfff783afbf11f892cd1625ea5814cf23f
Blaise <47299725+blaisepop@users.noreply.github.com>
2022-10-25 20:27:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Bubble Club  - Laverie Neuchâtel</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
